0e34f946abd0492163ddb6fad84c83d2bed9f560
tja1980 <tristan.addie@gmail.com>
2015-06-21 03:44:06 +0800

    add_column fixes
    Removed underscores, this was causing some issues previously with
    DBIx::Class

--- lib/Schema/Result/Watch.pm
+++ lib/Schema/Result/Watch.pm
@@ -3,7 +3,7 @@ use base qw/DBIx::Class::Core/;
 
 __PACKAGE__->load_components(qw/InflateColumn::DateTime/);
 __PACKAGE__->table('rss_watch');
-__PACKAGE__->add_columns(qw/id name has not smart_ep_filter last_seen/);
+__PACKAGE__->add_columns(qw/id name has not smartepfilter lastseen/);
 __PACKAGE__->set_primary_key('id');
 __PACKAGE__->has_many(smart_ep_filter => 'Schema::Result::SmartEpFilter', 'watch_id');
 
